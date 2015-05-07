2d978aa44c285ebe1b6cc924ee662a7f1b2c3025
Andrew Page <andrew.page32@gmail.com>
2015-05-07 17:37:37 -0400

    Updating authoring information.

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'A collection of utility functions for working with strings in JavaScript in the browser or Node.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/andyp22/slang',
+  git: 'https://github.com/devongovett/slang',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
